7d8d986e9e2a0dba832d0008823ca7c62e6546bc
Marco Bacis <marc.bacis@gmail.com>
2026-03-28 16:09:08 +0100

    feat: add encryption capabilities

--- src/Persistence/ApplicationDbContext.cs
+++ src/Persistence/ApplicationDbContext.cs
@@ -1,17 +1,22 @@
 using Microsoft.EntityFrameworkCore;
+using SoftFluent.EntityFrameworkCore.DataEncryption;
 
 namespace Persistence;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    private readonly IEncryptionProvider _encryptionProvider;
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IEncryptionProvider encryptionProvider)
         : base(options)
     {
+        _encryptionProvider = encryptionProvider;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        modelBuilder.UseEncryption(_encryptionProvider);
     }
 }
--- src/Persistence/Configuration/Gym.Configuration.cs
+++ src/Persistence/Configuration/Gym.Configuration.cs
@@ -1,6 +1,7 @@
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SoftFluent.EntityFrameworkCore.DataEncryption;
 
 namespace Persistence.Configuration;
 
@@ -11,5 +12,7 @@ public class GymConfiguration : IEntityTypeConfiguration<Gym>
         builder.ToTable(nameof(Gym));
         builder.HasKey(e => e.Id);
         builder.Property(e => e.Id).HasDefaultValueSql("uuidv7()");
+
+        builder.Property(e => e.Password).IsEncrypted();
     }
 }
